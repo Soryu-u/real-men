28281defa7e90feb1621ba025f7f6d01103f6c53
Soryu <collages447@gmail.com>
2022-06-17 16:52:17 +0300

    add post component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './Header.module.css'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <div className={styles.header}>
-
+      <Link className={styles.logoText} to='/'>vall-hall-a</Link>
     </div>
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,58 +1,32 @@
 import React from 'react'
 import styles from './Home.module.css'
 import poster from '../../images/poster.jpg'
+import posts from '../../data.json'
 
 import $ from 'jquery'
+import { Post } from '../modules/Post'
 
 export const Home = () => {
 
-  $(document).scroll(function() {
+  $(document).on('scroll', function() {
     if ($(this).scrollTop() > 190) {
-      $('.' + styles.poster).css({'webkit-filter':'blur(5px)', 'filter':'blur(5px)'});
+      $('.' + styles.poster).css({'webkit-filter':'blur(3px)', 'filter':'blur(3px)'});
       } else {
         $('.' + styles.poster).css({'webkit-filter':'', 'filter':''});
       }
-});
+  });
+
+  // const posts = ['zxc', 'zxc', 'cxz', 'asd', 'qwerty'];
 
   return (
-    <div className={styles.home}>
+    <div>
+      <div className={styles.home}>
         <img className={styles.poster} src={poster} alt='poster' />
+      </div>
         <div className={styles.content}>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
-            <div> zxc </div>
+            {posts.map( post => (
+              <Post key={ post.id } data={ post } />
+            ))}
         </div>
     </div>
   )
